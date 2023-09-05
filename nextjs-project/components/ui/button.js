7a7171e5b1754cbe0f1356594e43e96e28891bb6
Paskay18 <124031153+Paskay18@users.noreply.github.com>
2023-09-05 16:47:28 +0200

    The final parts of the app
    -Filters

--- nextjs-project/components/ui/button.js
+++ nextjs-project/components/ui/button.js
@@ -3,8 +3,13 @@ import classes from "./button.module.css"
 
 
 export default function Button (props) {
-    return <Link className={classes.btn} href= {props.link}>
+
+    if (props.link){
+        return <Link className={classes.btn} href= {props.link}>
         {props.children}
     </Link>
+    }
+ 
+    return <button className={classes.btn} onClick={props.onClick}>{props.children}</button>
 
 }
--- nextjs-project/pages/events/[...slug].js
+++ nextjs-project/pages/events/[...slug].js
@@ -1,7 +1,50 @@
+import { getFilteredEvents } from "@/dummy-data";
+import { useRouter } from "next/router"
+import EventList from "@/components/events/event-list";
+import ResultsTitle from "@/components/events/results-title";
+import { Fragment } from "react";
+
+
+
 export default function FilteredEventsPage () {
+
+    const router = useRouter();
+    const filterData  = router.query.slug;
+
+    if(!filterData){
+        return <p className="center">Loading...</p>
+    }
+
+    const filteredYear = filterData[0];
+    const filteredMonth = filterData[1];
+
+    const numYear = +filteredYear
+    const numMonth = +filteredMonth
+
+    if (isNaN(numYear) || isNaN(numMonth) || numYear >2030 || numYear<2021 || numMonth < 1 || numMonth >12  ){
+        return <p>Invalid filter. Please adjust your values</p>
+    }
+
+    const filteredEvents = getFilteredEvents({
+        year: numYear,
+        month: numMonth,
+    })
+    
+    if(!filteredEvents || filteredEvents.length === 0){
+      return ( <p>
+        No events found for the chosen filter!
+        </p>) 
+
+    }
+
+
+    const date = new Date(numYear, numMonth -1)
+    
     return (
-        <div>
-            <h1>Filtered Events</h1>
-        </div>
+        <Fragment >
+             <ResultsTitle  date={date} />
+                 <EventList items={filteredEvents} />
+        </Fragment>
+           
     )
 }
