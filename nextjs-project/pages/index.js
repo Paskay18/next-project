e1d8dca9f7bd39ca2c5439f317a9c2c8b25f0c16
Paskay18 <124031153+Paskay18@users.noreply.github.com>
2023-09-18 09:45:52 +0200

    fixed project

--- nextjs-project/pages/index.js
+++ nextjs-project/pages/index.js
@@ -1,18 +1,26 @@
-import { getFeaturedEvents } from "@/dummy-data"
-import EventList from "@/components/events/event-list";
+import {getFeaturedEvents} from '../components/helpers/api-util';
+import EventList from "../components/events/event-list";
 
-
-export default function HomePage() {
-
-  const featuredEvents = getFeaturedEvents();
-
+function HomePage(props) {
+  
+  
+  
   return (
-<div>
-   
-   <EventList items={featuredEvents} />
-</div>
-
-
-  )
-   
+    <div>
+      <EventList items={props.events} />
+    </div>
+  );
 }
+
+export async function getStaticProps() {
+ const featuredEvents = await getFeaturedEvents();
+ 
+  return {
+    props: {
+      events:featuredEvents
+    },
+    revalidate: 1800
+  }
+}
+
+export default HomePage;
